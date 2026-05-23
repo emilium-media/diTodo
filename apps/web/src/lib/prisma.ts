bc5c4e3da52d906cddcb9f833a578f4b48ad08cc
emilium-media <support@emilium.agency>
2026-05-23 00:28:32 -0300

    prisma 2

--- apps/web/src/lib/prisma.ts
+++ apps/web/src/lib/prisma.ts
@@ -1,12 +1,8 @@
 import { PrismaClient } from '@prisma/client'
 
-// Patrón Singleton para evitar agotar conexiones en desarrollo
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
-// Instanciamos Prisma de forma nativa pasando la URL dinámicamente
-export const prisma = globalForPrisma.prisma || new PrismaClient({
-    datasourceUrl: process.env.DATABASE_URL,
-})
+export const prisma = globalForPrisma.prisma || new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
